1fed9fbe8d6201a3446140d5f999162b408f2914
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-05 09:37:12 +0200

    test env

---
+++ 000_test_env/Test.java
@@ -0,0 +1,17 @@
+/* Test.java */
+
+// Test the development environment
+//
+// Compile:
+// $ javac Test.java
+//
+// Execute:
+// $ java Test
+
+public class Test
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Hello, World!");
+  }
+}
