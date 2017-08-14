2cc6e895d31fed4feb9d199d8c53c1f52872b5d9
Fabio Z Tessitore <fabioztessitore@gmail.com>
2017-08-14 10:33:27 +0200

    enhanced for

--- 002_array/Dice.java
+++ 002_array/Dice.java
@@ -6,23 +6,43 @@ import java.util.Random;
 
 public class Dice
 {
+  private static final int FACES = 6;
+  private static Random random;
+
   public static void main(String[] args)
   {
-    final int FACES = 6;
     int result[] = new int[FACES];
     int i;
+    int total = 0;
     final int n = 30000;
-    Random random = new Random();
+    random = new Random();
 
-    for (i = 0; i < result.length; i++) result[i] = 0;
+    init(result);
 
-    for (i = 0; i < n; i++) {
-      result[random.nextInt(FACES)]++;
-    }
+    simulate(result, n);
 
     System.out.println("  F     Occurrence");
     for (i = 0; i < result.length; i++) {
       System.out.printf("%3d%15d\n", i+1, result[i]);
+    }
+
+    for (int item : result) total += item;
+    System.out.printf("# of throws: %d\n", total);
+  }
+
+  private static void init(int[] result)
+  {
+    int i;
+
+    for (i = 0; i < result.length; i++) result[i] = 0;
+  }
+
+  private static void simulate(int[] result, int n)
+  {
+    int i;
+
+    for (i = 0; i < n; i++) {
+      result[random.nextInt(FACES)]++;
     }
   }
 }
